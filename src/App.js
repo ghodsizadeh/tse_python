c1ba895aefa1e4578574f5477d5b2081d8ea7ace
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2020-04-01 21:47:24 +0430

    add flow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
