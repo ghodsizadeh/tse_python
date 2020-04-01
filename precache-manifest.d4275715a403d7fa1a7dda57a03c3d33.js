60f654aa9895f85a3664778fbe710a5dc4b0a0dd
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2020-04-01 21:17:57 +0430

    Updates

---
+++ precache-manifest.d4275715a403d7fa1a7dda57a03c3d33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0812a6575f058af1e2b59a2da2b593",
+    "url": "/tse_python/index.html"
+  },
+  {
+    "revision": "7b8726f00e36336842cf",
+    "url": "/tse_python/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "03f4025eb26187f8ce30",
+    "url": "/tse_python/static/js/2.cbd67373.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tse_python/static/js/2.cbd67373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b8726f00e36336842cf",
+    "url": "/tse_python/static/js/main.80c14d3d.chunk.js"
+  },
+  {
+    "revision": "6f455028dfcc8fb82796",
+    "url": "/tse_python/static/js/runtime-main.8196a12b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tse_python/static/media/logo.5d5d9eef.svg"
+  }
+]);
